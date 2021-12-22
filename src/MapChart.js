5fea692feb716d277892582193007e424ccb3ed0
Yosef Alnajjar <yosefanajjar@gmail.com>
2021-12-22 21:19:04 +0300

    build: simple world map project

---
+++ src/MapChart.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect, memo } from "react";
+import { csv } from "d3-fetch";
+import { scaleLinear } from "d3-scale";
+import {
+  ZoomableGroup,
+  ComposableMap,
+  Geographies,
+  Geography,
+  Sphere,
+  Graticule
+} from "react-simple-maps";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+const colorScale = scaleLinear()
+  .domain([0.29, 0.68])
+  .range(["#ffedea", "#ff5233"]);
+
+const MapChart = ({ setTooltipContent }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    csv(`/vulnerability.csv`).then((data) => {
+      setData(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <ComposableMap
+        data-tip=""
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 147
+        }}
+      >
+        <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+        <ZoomableGroup zoom={1} center={[0, 0]}>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map(geo => {
+                const d = data.find((s) => s.ISO3 === geo.properties.ISO_A3);
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={d ? colorScale(d["2017"]) : "#F5F4F6"}
+                    onClick={() => window.open(`https://en.wikipedia.org/wiki/${geo.properties.NAME}`, '_blank')}
+                    onMouseEnter={() => {
+                      const { NAME } = geo.properties;
+                      setTooltipContent(NAME);
+                    }}
+                    onMouseLeave={() => {
+                      setTooltipContent("");
+                    }}
+                    style={{
+                      default: {
+                        outline: "none"
+                      },
+                      hover: {
+                        fill: "#F53",
+                        outline: "none"
+                      },
+                      pressed: {
+                        fill: "#E42",
+                        outline: "none"
+                      }
+                    }}
+                  />
+                )
+              }
+              )
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </>
+  );
+};
+
+export default memo(MapChart);
